5c1d05d862ef82f36d1a0aa4733d9243055f369d
SergioRubio <s.rtriguero@alumnos.upm.es>
2026-03-17 14:26:29 +0100

    errors

--- campusSostenible/src/main/java/com/kernelpanic/campusostenible/core/domain/WeatherData.java
+++ campusSostenible/src/main/java/com/kernelpanic/campusostenible/core/domain/WeatherData.java
@@ -1,9 +1,16 @@
 package com.kernelpanic.campusostenible.core.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
